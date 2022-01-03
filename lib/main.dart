c3283184db6e4666e1bd0e14929120a1c7381d46
植田大雅 <uetai0228@icloud.com>
2022-01-03 20:13:00 +0900

    L05-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 
+///////////////////////////////
+// ① Main：Flutterアプリもmain()からコードが実行されます。
+// `void main() => runApp(MyApp());` でも意味は同じです。
 void main() {
-  runApp(
-    Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          /// Text 1
-          Text(
-            'Hello, world!',
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          // Text 2
-          Text(
-            "hello all",
-            textDirection: TextDirection.ltr,
-          ),
-        ],
-      ),
-    ),
-  );
+  return runApp(MyApp());
+} 
+
+///////////////////////////////
+// ② アプリの基盤：アプリのテーマやスタイルを設定する。その上のページを追加していく。
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // MaterialAppという形式のアプリを作成
+    return MaterialApp(
+      theme: ThemeData(), // アプリのテーマカラーなど詳細を入力
+      home: MyHomePage(), // メインページを作成
+    );
+  }
+}
+
+///////////////////////////////
+// ③ アプリのメインページ（固定）
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Menu Bar"),
+        ),
+        body: Center(
+          child: Text("ABC"),
+        )
+    );
+  }
 }
