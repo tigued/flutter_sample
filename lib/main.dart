69038a5872ad7fccc8caaa0501952d0c2603e913
植田大雅 <uetai0228@icloud.com>
2022-01-03 20:27:32 +0900

    L05-4-クラス化

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 // `void main() => runApp(MyApp());` でも意味は同じです。
 void main() {
   return runApp(MyApp());
-} 
+}
 
 ///////////////////////////////
 // ② アプリの基盤：アプリのテーマやスタイルを設定する。その上のページを追加していく。
@@ -30,35 +30,36 @@ class MyHomePage extends StatelessWidget {
         title: Text("Menu Bar"),
       ),
       body: Center(
-        child: ListView( //<--追加箇所
-          children: [ //<--追加箇所
-            // <--　ここから1枚目のカード
-            Card(
-              margin: EdgeInsets.all(10),
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Checkbox(onChanged: null, value: false),
-                    Text("TODO 1"),
-                  ],
-                ),
-              ),
-            ),
-            // <--　ここから2枚目のカード
-            Card(
-              margin: EdgeInsets.all(10),
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Checkbox(onChanged: null, value: false),
-                    Text("TODO 2"),
-                  ],
-                ),
-              ),
-            ),
-          ]
+        child: ListView(children: [
+          // ここから下が修正箇所
+          TodoCardWidget(label: "TODO 1"),
+          TodoCardWidget(label: "TODO 2"),
+          TodoCardWidget(label: "TODO 3"),
+          TodoCardWidget(label: "TODO 4"),
+        ]),
+      ),
+    );
+  }
+}
+
+////////////////////
+// ④ TodoCardのWidget
+class TodoCardWidget extends StatelessWidget {
+  final String label;
+
+  TodoCardWidget({Key? key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          children: [
+            Checkbox(onChanged: null, value: false),
+            Text(this.label),
+          ],
         ),
       ),
     );
