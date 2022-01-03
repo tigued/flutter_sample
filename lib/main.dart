f173ff71aecb32653b5a898c9b80a1242a7db956
植田大雅 <uetai0228@icloud.com>
2022-01-03 21:03:06 +0900

    L07-1-モーションセンサ

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sensors/sensors.dart';
 
 ///////////////////////////////
 void main() => runApp(MyApp());
@@ -21,174 +20,55 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+// ⑧ MyHomePageの状態
 class _MyHomePageState extends State<MyHomePage> {
+  String data = "";
+  String label = "";
+  var color = Colors.white;
+
   @override
   void initState() {
     super.initState();
-  }
+    // ① initState()を上書きし、userAccelerometerセンサのイベントをモニタリングする
+    userAccelerometerEvents.listen((event) {
+      setState(() {
+        // ② センサデータの生データを可視化する
+        data = "X:${event.x.toStringAsFixed(3)}\n"
+            "Y:${event.y.toStringAsFixed(3)}\n"
+            "Z:${event.z.toStringAsFixed(3)}";
 
-  /// ---- ① 非同期にカードリストを生成する関数 ----
-  Future<List<dynamic>> getCards() async {
-    var prefs = await SharedPreferences.getInstance();
-    List<Widget> cards = [];
-    var todo = prefs.getStringList("todo") ?? [];
-    for (var jsonStr in todo) {
-      // JSON形式の文字列から辞書形式のオブジェクトに変換し、各要素を取り出し
-      var mapObj = jsonDecode(jsonStr);
-      var title = mapObj['title'];
-      var state = mapObj['state'];
-      cards.add(TodoCardWidget(label: title, state: state));
-    }
-    return cards;
+        // ③ 加速度データの値に応じて、labelとcolor(背景色)を変更する
+        if (event.x.abs() > 15) {
+          label = "X-Axis Event";
+          color = Colors.blueAccent;
+        }
+        if (event.y.abs() > 15) {
+          label = "Y-Axis Event";
+          color = Colors.redAccent;
+        }
+        if (event.z.abs() > 15) {
+          label = "Z-Axis Event";
+          color = Colors.greenAccent;
+        }
+      });
+    });
   }
-
-  /// ------------------------------------
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("My TODO"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                SharedPreferences.getInstance().then((prefs) async {
-                  await prefs.setStringList("todo", []);
-                  setState(() {});
-                });
-              },
-              icon: const Icon(Icons.delete))
-        ],
+        title: const Text("Menu Bar"),
       ),
-      body: Center(
-        /// ---- ② 非同期にカードリストを更新するには、FutureBuilder を使います----
-        child: FutureBuilder<List>(
-          future: getCards(), // <--- getCards()メソッドの実行状態をモニタリングする
-          builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return const Text('Waiting to start');
-              case ConnectionState.waiting:
-                return const Text('Loading...');
-              default:
-                // getCards()メソッドの処理が完了すると、ここが呼ばれる。
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  return ListView.builder(
-                      // リストの中身は、snapshot.dataの中に保存されているので、
-                      // 取り出して活用する
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return snapshot.data![index];
-                      });
-                }
-            }
-          },
-        ),
-
-        /// ------------------------------------
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          var label = await _showTextInputDialog(context);
-
-          if (label != null) {
-            SharedPreferences prefs = await SharedPreferences.getInstance();
-            var todo = prefs.getStringList("todo") ?? [];
-
-            // 辞書型オブジェクトを生成し、JSON形式の文字列に変換して保存
-            var mapObj = {"title": label, "state": false};
-            var jsonStr = jsonEncode(mapObj);
-            todo.add(jsonStr);
-            await prefs.setStringList("todo", todo);
-
-            setState(() {});
-          }
-        },
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-
-  final _textFieldController = TextEditingController();
-
-  Future<String?> _showTextInputDialog(BuildContext context) async {
-    return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text('TODO'),
-            content: TextField(
-              controller: _textFieldController,
-              decoration: const InputDecoration(hintText: "タスクの名称を入力してください。"),
-            ),
-            actions: <Widget>[
-              ElevatedButton(
-                child: const Text("キャンセル"),
-                onPressed: () => Navigator.pop(context),
-              ),
-              ElevatedButton(
-                child: const Text('OK'),
-                onPressed: () =>
-                    Navigator.pop(context, _textFieldController.text),
-              ),
+      body: Container(
+        color: color,
+        child: Center(
+          child: Column(
+            children: [
+              Text(data),
+              Text(label, style: const TextStyle(fontSize: 30)),
             ],
-          );
-        });
-  }
-}
-
-////////////////////
-class TodoCardWidget extends StatefulWidget {
-  final String label;
-  // 真偽値（Boolen）型のstateを外部からアクセスできるように修正
-  var state = false;
-
-  TodoCardWidget({
-    Key? key,
-    required this.label,
-    required this.state,
-  }) : super(key: key);
-
-  @override
-  _TodoCardWidgetState createState() => _TodoCardWidgetState();
-}
-
-class _TodoCardWidgetState extends State<TodoCardWidget> {
-  void _changeState(value) async {
-    setState(() {
-      widget.state = value ?? false;
-    });
-
-    // --- ③ ボタンが押されたタイミング状態を更新し保存する ---
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    var todo = prefs.getStringList("todo") ?? [];
-
-    for (int i = 0; i < todo.length; i++) {
-      var mapObj = jsonDecode(todo[i]);
-      if (mapObj["title"] == widget.label) {
-        mapObj["state"] = widget.state;
-        todo[i] = jsonEncode(mapObj);
-      }
-    }
-
-    prefs.setStringList("todo", todo);
-
-    /// ------------------------------------
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10),
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Row(
-          children: [
-            Checkbox(onChanged: _changeState, value: widget.state),
-            Text(widget.label),
-          ],
+          ),
         ),
       ),
     );
